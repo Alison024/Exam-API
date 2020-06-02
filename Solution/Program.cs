a3ac0da47622229c76fdd0bfbad1148813c58fd2
A <Andreyfox.ru@gmail.com>
2020-06-02 23:48:20 +0300

    Connection is complete !!!

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Solution.Persistence.Context;
 
 namespace Solution
 {
@@ -13,14 +16,17 @@ namespace Solution
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHost(args);
+            using(var scope = host.Services.CreateScope()){
+            using (var context = scope.ServiceProvider.GetService<AppDbContext>()){
+                context.Database.EnsureCreated();
+                }
+            }
+            //CreateWebHost(args).Build().Run();
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost CreateWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
     }
 }
